cfd5c05654abfbdc99161692e09478d409ac67d8
Sekwaila <sekwailajohn@gmail.com>
2026-08-01 19:32:45 +0200

    Add initial configuration for SEKWAILA OMEGA X

---
+++ config.py
@@ -0,0 +1,26 @@
+"""
+SEKWAILA OMEGA X
+Configuration
+"""
+
+APP_NAME = "SEKWAILA OMEGA X"
+
+VERSION = "4.0"
+
+SYMBOLS = [
+    "BTC/USD",
+    "XAU/USD",
+    "EUR/USD",
+]
+
+TIMEFRAME = "15min"
+
+SCAN_INTERVAL = 60
+
+TWELVEDATA_API_KEY = ""
+
+TELEGRAM_BOT_TOKEN = ""
+
+TELEGRAM_CHAT_ID = ""
+
+DATABASE_NAME = "omega_x.db"

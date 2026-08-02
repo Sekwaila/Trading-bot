a046843c5ad062c2da1d53c1e310ec643d962603
Sekwaila <sekwailajohn@gmail.com>
2026-08-02 12:25:39 +0200

    Add logging utility in logger.py
    Implement a logging utility with customizable logger.

---
+++ logger.py
@@ -0,0 +1,29 @@
+"""
+SEKWAILA OMEGA X
+Logging Utility
+"""
+
+import logging
+import sys
+
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] %(levelname)s | %(name)s | %(message)s",
+        datefmt="%H:%M:%S",
+    )
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    return logger
